3f4153279490c42951544e73346bfa97d5416f62
siekmanj <siekmanj@oregonstate.edu>
2018-09-21 19:09:03 -0700

    indentation, comments, clarity

--- examples/mlp_mnist.c
+++ examples/mlp_mnist.c
@@ -10,73 +10,61 @@
 #include <string.h>
 
 
-int main(int argc, char *argv[]){
+int main(void){
   srand(time(NULL));
 
-	if(argc < 2 || strlen(argv[1]) == 0){
-		printf("You must provide a valid filename.\n");
-		return -1;
-	}
-
 	MLP n = createMLP(28*28, 15, 10);
-	//addLayer(&n, 28*28); //input layer
-	//addLayer(&n, 15);
-	//addLayer(&n, 10); //output layer
+	n.plasticity = 0.2; //0.2 is a good starting learning rate - generally, the more layers/neurons, the lower your learning rate should be.
 
 	size_t epochs = 30;
 	int epoch = 0;
 
 	//Training data
 	{
+		printf("Training for %lu epochs.\n", epochs);
 		ImageSet training_set;
 		size_t height, width;
-		openImageSet(&training_set, 47040016, "../mnist/train-images-idx3-ubyte", "../mnist/train-labels-idx1-ubyte");
-
+		openImageSet(&training_set, 47040016, "../mnist/train-images-idx3-ubyte", "../mnist/train-labels-idx1-ubyte"); //You may need to provide your own mnist file
 		float avgCost = 0;
 
-		for(size_t i = 0; i < training_set.numImages * epochs; i++){
+		for(size_t i = 0; i < training_set.numImages * epochs; i++){ //Run for the given number of epochs
 			size_t index = i % training_set.numImages;
-			float* img = img2floatArray(&training_set, index, &height, &width);
-      int correctlabel = label(&training_set, index);
+			float* img = img2floatArray(&training_set, index, &height, &width); //Image is returned as a float array (must have same dims as input layer)
+			setInputs(&n, img); //Set the activations of the neurons in the input layer.
 
-			setInputs(&n, img);
-			float c = descend(&n, correctlabel);
+      int correctlabel = label(&training_set, index); //Retrieve the label from the image set.
 
-      if(isnan(c)){
-        printf("cost was nan: %f\n", c);
-        while(1);
-      }
+			float c = descend(&n, correctlabel); //Perform feedforward & backpropagation, and return cost
+
 			avgCost += c;
 
+			//Save the state of the network at the end of each epoch.
 			if(i % training_set.numImages == 0 && i != 0){
 				printf("Epoch %d finished, cost %f.\n", epoch++, avgCost/i);
-				char buff[100];
-				memset(buff, '\0', 100);
-				strcat(buff, "../saves/");
-				strcat(buff, argv[1]);
-				strcat(buff, ".mlp");
-        saveMLPToFile(&n, buff);
-        printf("\n**********\nlabel: %d, cost: %f, avgcost: %f\n", correctlabel, c, avgCost/i);
-			  printOutputs(n.output);
-				printWeights(n.output);
-				getchar();
+        saveMLPToFile(&n, "../saves/mnist_784_25_10.mlp");
 			}
 		}
-		printf("\nAvg training cost: %f / %u * %d = %f\n", avgCost, training_set.numImages, epoch, avgCost / (training_set.numImages*epochs));
 	}
+
 	//Testing data
 	{
 		printf("Testing:\n");
 		ImageSet testing_set;
+
 		openImageSet(&testing_set, 7840016, "../mnist/t10k-images-idx3-ubyte", "../mnist/t10k-labels-idx1-ubyte");
+
 		float avgCost = 0;
 		float avgCorrect = 0;
+
 		for(int i = 0; i < testing_set.numImages; i++){
 			size_t height, width;
-			float* img = img2floatArray(&testing_set, i, &height, &width);
-			setInputs(&n, img);
-			feedforward(&n);
-			int guess = bestGuess(&n);
+			float* img = img2floatArray(&testing_set, i, &height, &width); //Get image as float array
+			setInputs(&n, img); //Set the activations in the input layer
+
+			feedforward(&n); //Perform feedforward only, no backprop
+
+			int guess = bestGuess(&n); //Get network's best guess
+
 			avgCorrect += (guess == label(&testing_set, i));
 		}
 		printf("resulting avg correct: %f\n", avgCorrect/testing_set.numImages);
