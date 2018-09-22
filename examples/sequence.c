1cf494c84f1b83e6cb5c260c52bd17983055b9d0
siekmanj <siekmanj@oregonstate.edu>
2018-09-21 19:09:03 -0700

    indentation, comments, clarity
    Former-commit-id: 9c4a1c56437607a8a90c72289a679a4e3a447628
    Former-commit-id: 9d2c6734dc2496eba14f60370db03291e91eac27

--- examples/sequence.c
+++ examples/sequence.c
@@ -1,56 +1,55 @@
-#include <stdlib.h>
+/* Jonah Siekmann
+ * 7/24/2018
+ * In this file are some tests I've done with the network.
+ */
 #include <stdio.h>
-#include <time.h>
 #include <string.h>
 #include "RNN.h"
 
-//tests an RNN on a sequence like 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, .....
-//each number n is repeated n times
+/*
+ * This is a simple demonstration of something a generic neural network would find 
+ * difficult, but a RNN can do fairly easily.
+ * The network is trained to match a pattern which outputs a number n, n times.
+ * For instance, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4 .... and so on.
+ */
 
 int data[] = {
-             1,
-						 2, 2,
-						 3, 3, 3,
-						 4, 4, 4, 4,
- 						 5, 5, 5, 5, 5,
- 						 6, 6, 6, 6, 6, 6,
- 						 7, 7, 7, 7, 7, 7, 7,
- 						 8, 8, 8, 8, 8, 8, 8, 8,
- 						 9, 9, 9, 9, 9, 9, 9, 9, 9,
-             };
+	1,
+	2, 2,
+	3, 3, 3,
+	4, 4, 4, 4,
+	5, 5, 5, 5, 5,
+	6, 6, 6, 6, 6, 6,
+	7, 7, 7, 7, 7, 7, 7,
+	8, 8, 8, 8, 8, 8, 8, 8,
+	9, 9, 9, 9, 9, 9, 9, 9, 9,
+};
+
 int main(void){
-	RNN n = createRNN(10, 15, 15, 10);
-	int count = 0;
+	RNN n = createRNN(10, 15, 15, 10); //Create a network with 4 layers. Note that it's important that the input and output layers are both 10 neurons large.
+	n.plasticity = 0.2;
+
 	float cost = 0;
-	n.plasticity = 0.05;
-	for(int epoch = 0; epoch < 1000000; epoch++){
+	int count = 0;
+
+	for(int epoch = 0; epoch < 10000; epoch++){ //Train for 1000 epochs.
 		size_t len = sizeof(data)/sizeof(data[0]);
-		for(int i = 0; i < len; i++){
-			//Make a one-hot vector
+
+		for(int i = 0; i < len; i++){ //Run through the entirety of the training data.
+
+			//Make a one-hot vector and use it to set the activations of the input layer
 			float one_hot[10];
 			memset(one_hot, '\0', 10*sizeof(float));
 			one_hot[data[i]] = 1.0;
 			setOneHotInput(&n, one_hot); 
 			
-			int label = data[(i+1) % len];	
-			float c = step(&n, label);
+			int label = data[(i+1) % len]; //Use the next character in the sequence as the label	
+			float c = step(&n, label); //Perform feedforward and backprop.
 			cost += c;
 
 			count++;	
 		
-				printf("label: %d, input: %d, output: %d, cost: %5.2f, avgcost: %5.2f, correct: %d\n", label, data[i], bestGuess(&n), c, cost/count, bestGuess(&n) == label);
-			if(epoch > 100000){
-				printf("input vector:\n");
-				for(int j = 0; j < 10; j++) printf("%f, ", one_hot[j]); printf("\n");
-				printf("input layer:\n");
-				printOutputs(n.input);
-				printf("hidden layer:\n");
-				printOutputs((Layer*)n.input->output_layer);
-				getchar();
-			}
-		}
-		if(epoch % 100000 == 0){
-			getchar();
+			printf("label: %d, input: %d, output: %d, cost: %5.2f, avgcost: %5.2f, correct: %d\n", label, data[i], bestGuess(&n), c, cost/count, bestGuess(&n) == label);
 		}
 	}
 }
