b0340f0ad8e26b45a0fff1ff949e9ac855f609f7
Jonah Siekmann <siekmanj@oregonstate.edu>
2018-11-05 10:52:36 -0800

    pushing to remote so can fix on laptop

--- examples/genetic.c
+++ examples/genetic.c
@@ -9,29 +9,33 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define POOL_SIZE 30
+#define POOL_SIZE 50
 #define INPUT_DIMENSIONS 4
 #define OUTPUT_DIMENSIONS 16
-
+#define TRIALS 10
 MLP pool[POOL_SIZE];
 
 float fit_calc(MLP *n, int label){
 	float sum = 0;
 	for(int i = 0; i < n->output->size; i++){
-		if(i == label) sum += 1 - n->output->neurons[i].activation;
-		else sum += n->output->neurons[i].activation;
+		if(i == label){
+			sum += n->output->neurons[i].activation;
+		}
+		else{
+			sum -= n->output->neurons[i].activation;
+		}
 	}
 	return sum;
 }
 
 Neuron *neuron_lookup(MLP *n, int layer_idx, int neuron_idx){
-//	printf("  starting neuron lookup, %d, %d\n", layer_idx, neuron_idx);
+	printf("  starting neuron lookup, %d, %d\n", layer_idx, neuron_idx);
 	Layer *current = n->input;
 	for(int i = 0; i < layer_idx; i++){
-//		printf("	cycling thru %p\n", current->output_layer);
+		printf("	cycling thru %p\n", current->output_layer);
 		current = current->output_layer;
 	}
-//	printf("  returning neuron %d (size %lu)\n", neuron_idx, current->size);
+	printf("  returning neuron %d (size %lu)\n", neuron_idx, current->size);
 	return &current->neurons[neuron_idx];
 }
 
@@ -73,44 +77,38 @@ void print_layers(MLP *n){
 	printf("]\n");
 }
 MLP crossbreed(MLP *partner1, MLP *partner2){
+	printf("starting crossbreed...\n");
 	MLP ret = copy_mlp(partner1);
+	printf("copied partner1\n");
 
 	Layer *current = ret.input;
 	int layer_idx = 0;
-	while(layer_idx < 4){
-		ret.output->squish = current->squish;
+	while(current != NULL){
+		printf("doing layer %d...\n", layer_idx);
 		if(current->input_layer != NULL){
-
+			printf("	Considering %p\n", current);
 			for(int i = 0; i < current->size; i++){
 				Neuron *partner_neuron = neuron_lookup(partner2, layer_idx, i);
+				printf("got neuron %p (%d of %d), input layer %p\n", partner_neuron, i, current->size, current->input_layer);
 				for(int j = 0; j < current->input_layer->size; j++){
-					if(rand()&1) ret.output->neurons[i].weights[j] = partner_neuron->weights[j];
+					printf("considering weight %d\n", j);
+					printf("	%p, %p, ", ret.output, ret.output->neurons);
+					printf("	%p\n", partner_neuron);
+					if(0) ret.output->neurons[i].weights[j] = partner_neuron->weights[j];
+					printf("done with weight %d\n", j);
 				}
+				printf("done with loop\n");
 				ret.output->neurons[i].bias = current->neurons[i].bias;
-				if(rand()&1) ret.output->neurons[i].bias = partner_neuron->bias;
+				if(0) ret.output->neurons[i].bias = partner_neuron->bias;
 			}
 		}
 		layer_idx++;
 		current = current->output_layer;
+		printf("moving on to %p\n", current);
 	}
 	return ret;
 }
-/*
-int highest(MLP *pool, int len){
-	int highestone = 0;
-	for(int i = 0; i < len; i++){
-		if(arr[i] < arr[highestone]) highestone = i;
-	}
-	return highestone;
-}
-int lowest(float *arr, int len){
-	int lowestone = 0;
-	for(int i = 0; i < len; i++){
-		if(arr[i] < arr[lowestone]) lowestone = i;
-	}
-	return lowestone;
-}
-*/
+
 float similarity_score(MLP *a, MLP *b){
 	int total = 0;
 	int identical = 0;
@@ -136,52 +134,80 @@ float similarity_score(MLP *a, MLP *b){
 int comp(const void *mlp1, const void *mlp2){
 	MLP f1 = *(MLP*)mlp1;
 	MLP f2 = *(MLP*)mlp2;
-	if(f1.performance > f2.performance) return 1;
-	if(f1.performance < f2.performance) return -1;
+	if(f1.performance < f2.performance) return 1;
+	if(f1.performance > f2.performance) return -1;
 	else return 0;
 }
-
+float sumweights(Layer* layer){
+	float sum = 0;
+	for(int i = 0; i < layer->size; i++){
+		for(int j = 0; j < layer->input_layer->size; j++){
+			sum += layer->neurons[i].weights[j];
+		}
+	}
+	return sum;
+}
+void print_pool(MLP *pool, size_t len){
+	printf("pool: [");
+	for(int i = 0; i < len; i++){
+		printf("%7.5f", sumweights(pool[i].output));
+		if(i < len-1) printf(", ");
+	}
+	printf("]\n");
+}
 int main(void){	
 	srand(time(NULL));
 	// Create a pool of NN's with randomly initialized weights
 	for(int i = 0; i < POOL_SIZE; i++){
-		pool[i] = createMLP(INPUT_DIMENSIONS, 15, 15, OUTPUT_DIMENSIONS);
+		pool[i] = createMLP(INPUT_DIMENSIONS, 30, OUTPUT_DIMENSIONS);
 	}
 	float avg_fitness = 0;
 	float last_fitness = 0;
 	for(int i = 0; /* forever */; i++){ 
-		//Create a random 4-bit binary number
-		int bit0 = rand()%2==0;
-		int bit1 = rand()%2==0;
-		int bit2 = rand()%2==0;
-		int bit3 = rand()%2==0;
-		int ans = bit0 * pow(2, 0) + bit1 * pow(2, 1) + bit2 * pow(2, 2) + bit3 * pow(2, 3);
-
-		float arr[4] = {bit0, bit1, bit2, bit3}; //Input array (1 bit per input)
-
 		for(int j = 0; j < POOL_SIZE; j++){
 			MLP *n = &pool[j];
-			setInputs(n, arr);
-			feedforward(n); //Calculate outputs
-			n->performance = fit_calc(n, ans);
+			n->performance = 0;
+			for(int k = 0; k < TRIALS; k++){
+				//Create a random 4-bit binary number
+				int bit0 = rand()%2==0;
+				int bit1 = rand()%2==0;
+				int bit2 = rand()%2==0;
+				int bit3 = rand()%2==0;
+				int ans = bit0 * pow(2, 0) + bit1 * pow(2, 1) + bit2 * pow(2, 2) + bit3 * pow(2, 3);
+				float arr[4] = {bit0, bit1, bit2, bit3}; //Input array (1 bit per input)
+
+				setInputs(n, arr);
+				feedforward(n); //Calculate outputs
+				n->performance += fit_calc(n, ans)/TRIALS;
+			}
 		}
 		qsort(pool, POOL_SIZE, sizeof(MLP), comp); //Order pool by highest fitness
 		for(int j = 0; j < POOL_SIZE && (!(i%10000)); j++){
 			printf("%d: %f\n", j, pool[j].performance);
 		}
+//		printf("Pool before cull:\n");
+//		print_pool(pool, POOL_SIZE);
 		for(int j = POOL_SIZE/2; j < POOL_SIZE; j++){
+			printf("Killing %f due to subpar performance (%f)\n", sumweights(pool[j].output), pool[j].performance);
 			dealloc_network(&pool[j]);
+			printf("Dealloc done. now choosing parents\n");
 			MLP *parent1 = &pool[rand()%(POOL_SIZE/2)];
 			MLP *parent2 = &pool[rand()%(POOL_SIZE/2)];
+			printf("crossbreeding...\n");
 			pool[j] = crossbreed(parent1, parent2);
-			mutate(pool[j].output, 0.05, 0.001);
+			printf("crossbred. now mutating...\n");
+			mutate(pool[j].output, 0.05, 0.01);
+			printf("done\n");
 		}
+//		printf("Pool after cull:\n");
+//		print_pool(pool, POOL_SIZE);
 		float fitness = pool[0].performance;
 		avg_fitness += fitness;
 //		float improvement = 100 * ((last_fitness/fitness) - 1);
 		//Debug stuff
-		if(!(i % 10000)){
-			printf("CURRENTLY ON GENERATION %d, best fitness: %f, avg %f\n", i, pool[0].performance, avg_fitness/i);
+		if(!(i % 1)){
+			float similarity = similarity_score(&pool[0], &pool[POOL_SIZE-1]);
+			printf("CURRENTLY ON GENERATION %d, best fitness: %f, avg %f, similarity between best and worst network: %f\n", i, pool[0].performance, avg_fitness/i, similarity);
 			getchar();
 		}	
 	}
