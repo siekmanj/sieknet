ac5567970ab302856c660fbf609ccff6188f7cf0
siekmanj <siekmanj@oregonstate.edu>
2018-12-22 14:15:41 -0800

    starting work on LSTM
    Former-commit-id: 6d78745ae9423571df4df3e489910982cca123fe
    Former-commit-id: 3e1e8efedfb89bb079c62bc38d391a3f755dbaff

--- networks/LSTM/LSTM.h
+++ networks/LSTM/LSTM.h
@@ -3,22 +3,41 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include "RNN.h"
+#include "MLP.h"
 
 
 // some magic to allow arbitrary numbers of parameters
-#define createRNN(...) rnn_from_arr((size_t[]){__VA_ARGS__}, sizeof((size_t[]){__VA_ARGS__})/sizeof(size_t))
+#define createLSTM(...) lstm_from_arr((size_t[]){__VA_ARGS__}, sizeof((size_t[]){__VA_ARGS__})/sizeof(size_t))
 
 //typedef MLP LSTM;
 typedef struct cell{
- 
+	Neuron input_activation;
+	Neuron input_gate;
+	Neuron forget_gate;
+	Neuron output_gate;
 
+} Cell;
+
+typedef struct lstm_layer{
+	Cell *cells;
+	struct Layer *input_layer;
+	struct Layer *output_layer;
+	void (squish)(struct Layer*);
+} Cell_Layer;
+
+typedef struct lstm{
+	Layer *input;
+	Layer *output;
+	Cell_Layer *hidden;
+	double plasticity;
+	double performance;
 } LSTM;
 
-LSTM lstm_from_arr(size_t arr[], size_t size);
+LSTM createLSTM(size_t input_dimension, size_t cells, size_t output_dimension);
 LSTM loadLSTMFromFile(const char *filename);
 
 void feedforward_forget(LSTM *n);
-float step(LSTM *n, int label;
+void process_cell(Cell *c);
+//float step(LSTM *n, int label;
 
 #endif
