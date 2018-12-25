ed42f7ff0729965af1cb615da3cd1e4b0203046a
siekmanj <siekmanj@oregonstate.edu>
2018-12-25 01:40:53 -0800

    attempting to fix backprop
    Former-commit-id: 5f7392eef8535884bff555088b1e87c618e1ca23
    Former-commit-id: 6fbb7e46e139efd02b44d8367416cd7f5d2ce857

--- networks/LSTM/LSTM.c
+++ networks/LSTM/LSTM.c
@@ -6,19 +6,94 @@
 #include "LSTM.h"
 #include <math.h>
 #include <string.h>
+
+#define ALLOCATE(TYPE, NUM) (TYPE*)malloc(NUM * sizeof(TYPE));
+
+void printlist(float *arr, size_t len){
+	printf("[");
+	for(int i = 0; i < len; i++){
+		printf("%6.5f", arr[i]);
+		if(i < len-1) printf(", ");
+		else printf("]\n");
+	}
+}
+
+Gate createGate(float *weights, float *bias, size_t sequence_length){
+	Gate g;
+	g.output = ALLOCATE(float, sequence_length);
+	g.dOutput = ALLOCATE(float, sequence_length);
+	g.gradient = ALLOCATE(float, sequence_length);
+
+	g.weights = weights;
+	g.bias = bias;
+
+	return g;
+}
+
+void reset_inputs(float **arr, size_t sequence_length, size_t input_dimension){
+	for(int i = 0; i < sequence_length; i++){
+		for(int j = 0; j < input_dimension; j++){
+			arr[i][j] = 0.0;
+		}
+	}
+}
+
 //Thanks to Arun Mallya for an excellent writeup on the backprop for an lstm
 //http://arunmallya.github.io/writeups/nn/lstm/index.html
 
-/*
- * Description: Initializes a long short-term memory network object.
- */ 
-static LSTM initLSTM(){
+LSTM createLSTM(size_t input_dim, size_t size){
 	LSTM n;
-	n.cells = NULL;
-	n.last_out = NULL;
-	n.input_dimension = 0;
-	n.size = 0;
-	n.plasticity = 0.01;
+	n.input_dimension = input_dim + size;
+
+	Cell *cells = (Cell*)malloc(size*sizeof(Cell));
+	for(int i = 0; i < size; i++){
+		Cell *cell = &cells[i];
+		
+		//allocate weights for this cell
+		float *input_nonl_weights = ALLOCATE(float, n.input_dimension);
+		float *input_gate_weights = ALLOCATE(float, n.input_dimension);
+		float *forget_gate_weights = ALLOCATE(float, n.input_dimension);
+		float *output_gate_weights = ALLOCATE(float, n.input_dimension);
+
+		//ditto for biases
+		float *input_nonl_bias = ALLOCATE(float, 1);
+		float *input_gate_bias = ALLOCATE(float, 1);
+		float *forget_gate_bias = ALLOCATE(float, 1);
+		float *output_gate_bias = ALLOCATE(float, 1);
+		
+		//randomly initialize weights
+		for(int j = 0; j < n.input_dimension; j++){
+			input_nonl_weights[j] = ((float)(rand()%7000)-3500)/10000;
+			input_gate_weights[j] = ((float)(rand()%7000)-3500)/10000;
+			forget_gate_weights[j] = ((float)(rand()%7000)-3500)/10000;
+			output_gate_weights[j] = ((float)(rand()%7000)-3500)/10000;
+		}
+		
+		//randomly initialize biases
+		*input_nonl_bias = ((float)(rand()%7000)-3500)/10000;
+		*input_gate_bias = ((float)(rand()%7000)-3500)/10000;
+		*forget_gate_bias = ((float)(rand()%7000)-3500)/10000;
+		*output_gate_bias = ((float)(rand()%7000)-3500)/10000;
+
+		//Allocate gates
+		cell->input_nonl = createGate(input_nonl_weights, input_nonl_bias, UNROLL_LENGTH);
+		cell->input_gate = createGate(input_gate_weights, input_gate_bias, UNROLL_LENGTH);
+		cell->forget_gate = createGate(forget_gate_weights, forget_gate_bias, UNROLL_LENGTH);
+		cell->output_gate = createGate(output_gate_weights, output_gate_bias, UNROLL_LENGTH);
+
+		cell->output = ALLOCATE(float, UNROLL_LENGTH);
+		cell->state = ALLOCATE(float, UNROLL_LENGTH);
+		cell->dstate = ALLOCATE(float, UNROLL_LENGTH);
+		cell->gradient = ALLOCATE(float, UNROLL_LENGTH);
+
+	}
+	n.cells = cells;
+	n.size = size;
+	n.hidden = ALLOCATE(float, n.input_dimension);
+	n.inputs = ALLOCATE(float*, UNROLL_LENGTH);
+	for(int t = 0; t < UNROLL_LENGTH; t++) n.inputs[t] = ALLOCATE(float, n.input_dimension);
+	reset_inputs(n.inputs, UNROLL_LENGTH, n.input_dimension);
+	n.t = 0;
 	return n;
 }
 
@@ -43,60 +118,77 @@ float inner_product(float *x, float *y, size_t length){
 }
 
 	
+void step(LSTM *n, float *input, float *desired){
+	if(n->t >= UNROLL_LENGTH || desired == NULL){ //We've reached the max unroll length, so time to do bptt
+		//bptt
+		for(int j = 0; j < n->size; j++){
+			Cell *c = &n->cells[j];
+			Gate *a = &c->input_nonl;
+			Gate *i = &c->input_gate;
+			Gate *f = &c->forget_gate;
+			Gate *o = &c->output_gate;
+			for(int t =
 
-/*
- * input MUST BE SAME SIZE AS n.input_dimension
- */
-void feedforward_forget(LSTM *n, float *input){
+		}
+		//end bptt
+		n->t = 0;
+		reset_inputs(n->inputs, UNROLL_LENGTH, n->input_dimension);
+	}
+	size_t t = n->t;
+	for(int i = 0; i < n->input_dimension - n->size; i++){
+		n->inputs[t][i] = input[i];
+	}
+	for(int i = n->input_dimension - n->size; i < n->input_dimension; i++){
 
-	//Create a new input vector containing last timestep's recurrent outputs, concatenate with old input vector
-	for(int i = 0; i < n->input_dimension; i++) n->inputs[i] = input[i];
-	for(int i = n->input_dimension; i < n->input_dimension + n->size; i++) n->inputs[i] = n->last_out[i-n->input_dimension];
+		if(t) n->inputs[t][i] = n->cells[i - (n->input_dimension - n->size)].output[t-1]; //recurrent input
+		else  n->inputs[t][i] = n->cells[i - (n->input_dimension - n->size)].output[UNROLL_LENGTH-1];
+	}
+	
+	printf("concatenated inputs: [");
+	for(int j = 0; j < n->input_dimension; j++){
+		printf("%4.3f", n->inputs[t][j]);
+		if(j < n->input_dimension-1) printf(", ");
+		else printf("]\n");
+	}
 
-	printf("last output: [");
+	//feedforward
 	for(int j = 0; j < n->size; j++){
-		printf("%4.3f", n->last_out[j]);
+		Cell *c = &n->cells[j];
+		Gate *a = &c->input_nonl;
+		Gate *i = &c->input_gate;
+		Gate *f = &c->forget_gate;
+		Gate *o = &c->output_gate;
+
+		a->output[t] = hypertan_element(inner_product(a->weights, n->inputs[t], n->input_dimension) + *a->bias);
+		i->output[t] = sigmoid_element(inner_product(i->weights, n->inputs[t], n->input_dimension) + *i->bias);
+		f->output[t] = sigmoid_element(inner_product(f->weights, n->inputs[t], n->input_dimension) + *f->bias);
+		o->output[t] = sigmoid_element(inner_product(o->weights, n->inputs[t], n->input_dimension) + *o->bias);
+
+
+		a->dOutput[t] = 1 - a->output[t] * a->output[t];
+		i->dOutput[t] = i->output[t] * (1 - i->output[t]);
+		f->dOutput[t] = f->output[t] * (1 - f->output[t]);
+		o->dOutput[t] = o->output[t] * (1 - o->output[t]);
+
+		if(t) c->state[t] = a->output[t] * i->output[t] + f->output[t] * c->state[t-1];
+		else c->state[t] = a->output[t] * i->output[t];
+		c->output[t] = hypertan_element(c->state[t]) * o->output[t];
+		printf("CELL %d: a_%d: %6.5f, i_%d: %6.5f, f_%d: %6.5f, o_%d: %6.5f, state %6.5f\n", j, t, a->output[t], t, i->output[t], t, f->output[t], t, o->output[t], c->state[t]);
+	}
+	//end feedforward
+	printf("output: [");
+	for(int j = 0; j < n->size; j++){
+		printf("%4.3f", n->cells[j].output[t]);
 		if(j < n->size-1) printf(", ");
 		else printf("]\n");
 	}
 
-	printf("concatenated inputs: [");
-	for(int j = 0; j < (n->size + n->input_dimension); j++){
-		printf("%4.3f", n->inputs[j]);
-		if(j < (n->size + n->input_dimension)-1) printf(", ");
-		else printf("]\n");
-	}
-
-	for(int i = 0; i < n->size; i++){
-		Cell *c = &n->cells[i];
-		Neuron *inpt_actv = &c->input_activation;
-		Neuron *inpt_gate = &c->input_gate;
-		Neuron *frgt_gate = &c->forget_gate;
-		Neuron *otpt_gate = &c->output_gate;
-
-	 	inpt_actv->input = inner_product(inpt_actv->weights, n->inputs, n->input_dimension) + inpt_actv->bias;
-		inpt_gate->input = inner_product(inpt_gate->weights, n->inputs, n->input_dimension) + inpt_gate->bias;
-		frgt_gate->input = inner_product(frgt_gate->weights, n->inputs, n->input_dimension) + frgt_gate->bias;
-		otpt_gate->input = inner_product(otpt_gate->weights, n->inputs, n->input_dimension) + otpt_gate->bias;
-
-		inpt_actv->activation = hypertan_element(inpt_actv->input);
-		inpt_gate->activation = sigmoid_element(inpt_gate->input);
-		frgt_gate->activation = sigmoid_element(frgt_gate->input);
-		otpt_gate->activation = sigmoid_element(otpt_gate->input);
-
-		inpt_actv->dActivation = 1 - inpt_actv->activation * inpt_actv->activation;
-		inpt_gate->dActivation = inpt_gate->activation * (1 - inpt_gate->activation);
-		frgt_gate->dActivation = frgt_gate->activation * (1 - frgt_gate->activation);
-		otpt_gate->dActivation = otpt_gate->activation * (1 - otpt_gate->activation);
-
-		c->lstate = c->state;
-		c->state = inpt_actv->activation * inpt_gate->activation + frgt_gate->activation * c->state;
-		c->output = hypertan_element(c->state) * otpt_gate->activation;
-		n->last_out[i] = c->output;
-		printf("	cell %d output was %5.3f, last_out[%d] is now %5.3f, input act of %5.3f, input gate of %5.3f, forget gate of %5.3f, output gate of %5.3f\n", i, c->output, i,  n->last_out[i], inpt_actv->activation, inpt_gate->activation, frgt_gate->activation, otpt_gate->activation);
-	}
+	printf("finished feedforward\n");
+	n->t++;
 }
 
+
+/*
 static float cost(LSTM *n, int label){
 	//cost
 	float sum = 0;
@@ -112,10 +204,10 @@ static float cost(LSTM *n, int label){
 	}
 	return sum;
 }
-
+*/
 /*
  * Single-layer backprop for now
- */
+ *
 float backpropagate_cells(LSTM *n, int label){
 	float c = cost(n, label);
 	
@@ -139,57 +231,20 @@ float backpropagate_cells(LSTM *n, int label){
 
 		cell->ldstate = cell->dstate * frgt_gate->activation;
 		
+		printf("	Cell %d: state %6.5f, dstate %6.5f, lstate %6.5f, ldstate %6.5f\n", i, cell->state, cell->dstate, cell->lstate, cell->ldstate);
+		
 		//Weight nudges
 		for(int j = 0; j < n->input_dimension; j++){
-			inpt_actv->weights[j] += inpt_actv->gradient * inpt_actv->dActivation * n->inputs[j];
-			inpt_gate->weights[j] += inpt_gate->gradient * inpt_gate->dActivation * n->inputs[j];
-			frgt_gate->weights[j] += frgt_gate->gradient * frgt_gate->dActivation * n->inputs[j];
-			otpt_gate->weights[j] += otpt_gate->gradient * otpt_gate->dActivation * n->inputs[j];
+			inpt_actv->weights[j] += inpt_actv->gradient * inpt_actv->dActivation * n->inputs[j] * n->plasticity;
+			inpt_gate->weights[j] += inpt_gate->gradient * inpt_gate->dActivation * n->inputs[j] * n->plasticity;
+			frgt_gate->weights[j] += frgt_gate->gradient * frgt_gate->dActivation * n->inputs[j] * n->plasticity;
+			otpt_gate->weights[j] += otpt_gate->gradient * otpt_gate->dActivation * n->inputs[j] * n->plasticity;
 		}
-		inpt_actv->bias += inpt_actv->gradient * inpt_actv->dActivation;
-		inpt_gate->bias += inpt_gate->gradient * inpt_gate->dActivation;
-		frgt_gate->bias += frgt_gate->gradient * frgt_gate->dActivation;
-		otpt_gate->bias += otpt_gate->gradient * otpt_gate->dActivation;
+		inpt_actv->bias += inpt_actv->gradient * inpt_actv->dActivation * n->plasticity;
+		inpt_gate->bias += inpt_gate->gradient * inpt_gate->dActivation * n->plasticity;
+		frgt_gate->bias += frgt_gate->gradient * frgt_gate->dActivation * n->plasticity;
+		otpt_gate->bias += otpt_gate->gradient * otpt_gate->dActivation * n->plasticity;
 	}
 	return c;
 }
-
-LSTM createLSTM(size_t size, size_t input_dim){
-	LSTM n = initLSTM();
-	size_t input_dimension = input_dim + size;
-	Cell *cells = (Cell*)malloc(size*sizeof(Cell));
-	for(int i = 0; i < size; i++){
-		//allocate weights for this cell
-		float *input_activation_weights = (float*)malloc(input_dimension*sizeof(float));
-		float *input_gate_weights = (float*)malloc(input_dimension*sizeof(float));
-		float *forget_gate_weights = (float*)malloc(input_dimension*sizeof(float));
-		float *output_gate_weights = (float*)malloc(input_dimension*sizeof(float));
-		for(int j = 0; j < input_dimension; j++){
-			input_activation_weights[j] = ((float)(rand()%7000)-3500)/10000;
-			input_gate_weights[j] = ((float)(rand()%7000)-3500)/10000;
-			forget_gate_weights[j] = ((float)(rand()%7000)-3500)/10000;
-			output_gate_weights[j] = ((float)(rand()%7000)-3500)/10000;
-		}
-		cells[i].input_activation.weights = input_activation_weights;
-		cells[i].input_gate.weights = input_gate_weights;
-		cells[i].forget_gate.weights = forget_gate_weights;
-		cells[i].output_gate.weights = output_gate_weights;
-
-		cells[i].input_activation.bias = ((float)(rand()%7000)-3500)/10000;
-		cells[i].input_gate.bias = ((float)(rand()%7000)-3500)/10000;
-		cells[i].forget_gate.bias = ((float)(rand()%7000)-3500)/10000;
-		cells[i].output_gate.bias = ((float)(rand()%7000)-3500)/10000;
-
-		cells[i].state = 0;
-		cells[i].output = 0;
-		cells[i].dstate = 0.01;
-		cells[i].gradient = 0.01;
-	
-	}
-	n.cells = cells;
-	n.last_out = (float*)malloc(size * sizeof(float));
-	n.size = size;
-	n.input_dimension = input_dim;
-	n.inputs = (float*)malloc((n.size + n.input_dimension) * sizeof(float));
-	return n;
-}
+*/
