542e6b3e13e09834fa1fcb7d7951c73279d0d239
siekmanj <siekmanj@oregonstate.edu>
2018-12-25 01:40:53 -0800

    attempting to fix backprop

--- networks/LSTM/LSTM.h
+++ networks/LSTM/LSTM.h
@@ -3,33 +3,39 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include "MLP.h"
 
+#ifndef UNROLL_LENGTH
+#define UNROLL_LENGTH 2
+#endif
 
-//typedef MLP LSTM;
+typedef struct gate{
+	float *output;
+	float *dOutput;
+	float *gradient;
+	float *weights;
+	float *bias;
+
+} Gate;
+
 typedef struct cell{
-	Neuron input_activation;
-	Neuron input_gate;
-	Neuron forget_gate;
-	Neuron output_gate;
-	float output;
-	float state;
-	float dstate;
-	float lstate;
-	float ldstate;
-	float gradient;
+	Gate input_nonl;
+	Gate input_gate;
+	Gate forget_gate;
+	Gate output_gate;
+	float *output;
+	float *state;
+	float *dstate;
+	float *gradient;
 } Cell;
 
 typedef struct lstm_layer{
 	Cell *cells;
-	float *inputs;
-	float *last_out;
+	float *hidden;
+	float **inputs;
 	size_t input_dimension;
 	size_t size;
+	size_t t;
 	double plasticity;
-//	struct Layer *input_layer;
-//	struct Layer *output_layer;
-//	void (squish)(struct Layer*);
 } LSTM;
 
 LSTM createLSTM(size_t input_dimension, size_t cells);
@@ -37,6 +43,7 @@ LSTM createLSTM(size_t input_dimension, size_t cells);
 
 void feedforward_forget(LSTM *n, float *);
 float backpropagate_cells(LSTM *, int);
+void step(LSTM *, float *, float *);
 void process_cell(Cell *c);
 //float step(LSTM *n, int label;
 
