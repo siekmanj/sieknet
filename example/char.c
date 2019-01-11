1bd9a5a3a76235ccfd1bf6ebecc5fd53d4dadb97
Jonah Siekmann <siekmanj@oregonstate.edu>
2019-01-11 12:16:17 -0800

    preparing for rewrites

---
+++ example/char.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
+#include "LSTM.h"
+
+
+#define CREATEONEHOT(name, size, index) float name[size]; memset(name, '\0', size*sizeof(float)); name[index] = 1.0;
+
+typedef uint8_t bool;
+
+/*
+ * This file is for training an LSTM character-by-character on any text (ascii) file provided.
+ */
+ 
+//Calling convention: ../bin/char [load/new] [path_to_modelfile] [path_to_txt_file] [epochs] [learning_rate] [sample/train] [stateful]
+
+
+static inline char int2char(int i){
+	if(i==95) return '\n';
+	return i+32;
+}
+static inline int char2int(char c){
+	if(c=='\n') return 95;
+	return c-32;
+}
+void bad_args(char *s){
+	printf("bad argument '%s'\n", s);
+	exit(1);
+}
+
+int main(int argc, char** argv){
+	srand(time(NULL));
+	setbuf(stdout, NULL);
+	if(argc < 2){ printf("must provide %d args.\n", 2); exit(1);}
+
+	printf("received %d args: [", argc);
+	for(int i = 0; i < argc; i++){
+		printf("'%s'", argv[i]);
+		if(i < argc-1)printf(", ");
+		else printf("]\n");
+	}
+	bool newlstm;
+	if(!strcmp(argv[1], "load")) newlstm = 0;
+	else if(!strcmp(argv[1], "new")) newlstm = 1;
+	else bad_args(argv[1]);
+
+	size_t ascii_range = 96; //96 useful characters in ascii: A-Z, a-z, 0-9, !@#$%...etc
+
+	printf("range: %lu\n", ascii_range);
+	for(int i = 0; i < ascii_range; i++){
+		printf("index %d has: '%c'\n", i, int2char(i));
+	}
+
+}
