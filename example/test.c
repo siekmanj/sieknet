cfb0ea936e6757a0c6430f0e8bda07c48e718168
Jonah Siekmann <siekmanj@oregonstate.edu>
2019-02-25 22:55:20 -0800

    (adding in a quick test file)
    Former-commit-id: 4b3349be39d4392acf005d428e1acab002fac90e
    Former-commit-id: 3e6cd1b43114810b6116c8732fecef1864fb11f7

---
+++ example/test.c
@@ -0,0 +1,30 @@
+/* Jonah Siekmann
+ * 1/20/2019
+ * This is a toy problem - training an mlp to convert a 4-bit binary string to a decimal number between 0 and 15.
+ */
+
+#include <mlp.h>
+#include <optimizer.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+#define CREATEONEHOT(name, size, index) float name[size]; memset(name, '\0', size*sizeof(float)); name[index] = 1.0;
+
+float uniform(float minimum, float maximum){
+	float center = minimum + (maximum - minimum)/2;
+	float max_mag = maximum - center;
+	if(rand()&1)
+		return center + ((((float)rand())/RAND_MAX)) * max_mag;
+	else
+		return center - ((((float)rand())/RAND_MAX)) * max_mag;
+}
+int main(){
+	srand(time(NULL));
+
+	MLP n = create_mlp(4, 6);
+
+	mlp_forward(&n, x);
+	//float cost = mlp_cost(&n, y);
+	//mlp_backward(&n);
+}
