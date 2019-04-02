9926661c3b08d928921913699c4fd8e0205b4ea5
Jonah Siekmann <siekmanj@oregonstate.edu>
2019-04-02 01:02:25 -0700

    fixed bug where gradient wasnt clipped correctly in cpu lstm code
    Former-commit-id: d6da18366f36ae1744a242951f7f8bdbd629f61d
    Former-commit-id: f5de8507ab46bf7d970e2e81f31da2eb46988648

--- include/conf.h
+++ include/conf.h
@@ -6,14 +6,14 @@
 /*
  * Print debug information during execution.
  */
-#define SIEKNET_DEBUG
+//#define SIEKNET_DEBUG
 
 /*
  * In noisy datasets, it can be necessary to clip gradients to avoid destructive parameter
  * updates. SIEKNET_MAX_GRAD will bound the maximum gradient wrt any parameter to the below
  * value.
  */
-#define SIEKNET_MAX_GRAD 0.10f //the max gradient of any parameter across all network types
+#define SIEKNET_MAX_GRAD 0.5f //the max gradient of any parameter across all network types
 
 /*
  * LSTM's can sometimes suffer from the exploding gradient problem. In my experience,
