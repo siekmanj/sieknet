b849180aa506f164c669fd7a7800f82129d6628c
Jonah Siekmann <siekmanj@oregonstate.edu>
2019-04-19 17:09:10 -0700

    Added linear logistic function, clock experiments continue
    Former-commit-id: 0db3d5b3dcf0ec71e9ba076d60d57ddfea559306
    Former-commit-id: 8d357fe42a417d7a661b3084fbaa34d150211b2c

--- example/clock.c
+++ example/clock.c
@@ -1,63 +1,54 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include <rnn.h>
+#include <lstm.h>
 #include <lstm.h>
 #include <optimizer.h>
 
 float PI = 3.14159;
 
 int main(){
-  LSTM n = create_lstm(1, 2, 1);
-  Momentum o = create_optimizer(Momentum, n);
-  o.alpha = 0.001;
+  srand(1);
+  float best_cost = 1.0f;
+  for(int l = 1; l < 10000; l+=100){
+    float lr = (float)l/10000;
+    for(int t = 4; t < 50; t++){
 
-  n.seq_len = 100; // one sin period
-  n.stateful = 1;
+      LSTM n = create_lstm(0, 5, 1);
+      n.seq_len = t;
+      n.output_layer.logistic = hypertan;
+      n.cost_fn = quadratic;
+      SGD o = create_optimizer(SGD, n);
+      o.learning_rate = lr;
+      
+      float avg_cost = 0;
+      printf("trying %f and %lu\n", lr, n.seq_len);
+      for(int i = 0; i < n.seq_len * 1000; i++){
 
-  n.output_layer.logistic = hypertan;
-  n.cost_fn = quadratic;
+        float y[] = {0.5 * cos((4 * PI * i )/(float)n.seq_len)};
 
-  float avg_cost = 0;
-  for(int i = 0; i < 10000 * n.seq_len; i++){
-    float *x = malloc(sizeof(float));
+        lstm_forward(&n, NULL);
+        avg_cost += lstm_cost(&n, y);
+        lstm_backward(&n);
 
-    if(!(i % n.seq_len))
-      *x = 0.0;
-    else
-      *x = 0.0;
-
-    //float x[] = {((float)rand())/RAND_MAX};
-    //float x[] = {0.0};
-    //float x[] = {cos(2 * PI * i / (n.seq_len))};
-
-    //float y[] = {0.5 * sin(2 * PI * i / (n.seq_len))};
-    float y[] = {(float)(i&1) - 0.5};
-
-    lstm_forward(&n, x);
-    avg_cost += lstm_cost(&n, y);
-    lstm_backward(&n);
-
-    float guess = n.output[0];
-    
-    if(!n.t) o.step(o);
-
-    if(!n.t){
-      avg_cost /= 1000;
-      printf("cost: %f             \r", avg_cost);
-      avg_cost = 0;
-      n.layers[0].loutput[0] = 1.0;
+        float guess = n.output[0];
+        
+        if(!n.t) o.step(o);
+      }
+      avg_cost /= n.seq_len * 1000;
+      printf("resulting cost: %f\n", avg_cost);
+      if(avg_cost < best_cost){
+        printf("%f: new best config found: lr %f and sequence len %d\n", avg_cost, lr, n.seq_len);
+        best_cost = avg_cost;
+        for(int i = 0; i < 60; i++){
+          float x[] = {0.0};
+          lstm_forward(&n, x);
+          float guess = n.output[0];
+          printf("%f\n", guess);
+        }
+        getchar();
+      }
+      dealloc_lstm(&n);
     }
-    free(x);
   }
-  printf("\n");
-
-  for(int i = 0; i < 60; i++){
-    float x[] = {0.0};
-    lstm_forward(&n, x);
-    float guess = n.output[0];
-    printf("%f\n", guess);
-  }
-
-
 }
