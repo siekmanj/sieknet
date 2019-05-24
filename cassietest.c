e8afa079c4358dde488ec01d7e70621ef95d0624
siekmanj <siekmanj@oregonstate.edu>
2019-05-24 16:27:45 -0700

    .

---
+++ cassietest.c
@@ -0,0 +1,18 @@
+#include <cassie_env.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	Environment env = create_cassie_env();
+	float *x = calloc(env.action_space, sizeof(float));
+	/*
+	while(1){
+		printf("stepping!\n");
+		env.step(env, x);
+		printf("rendering!\n");
+		//env.render(env);
+
+	}
+	*/
+	printf("Done.\n");
+}
